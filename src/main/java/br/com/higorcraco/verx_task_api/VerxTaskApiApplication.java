912068d237d1c4b147ff0318be289c6cd215e080
Higor Craco <higorcraco@gmail.com>
2026-03-06 19:19:51 -0300

    criado projeto

---
+++ src/main/java/br/com/higorcraco/verx_task_api/VerxTaskApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.higorcraco.verx_task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerxTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerxTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/higorcraco/verx_task_api/VerxTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.higorcraco.verx_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerxTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
