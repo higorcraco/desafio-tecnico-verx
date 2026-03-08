7b6bbb491b5d2897d08bbe32d2c96a5db0c4d9e2
Higor Craco <higorcraco@gmail.com>
2026-03-08 13:55:05 -0300

    Ajustado validação title UpdateTaskRequest

--- src/main/java/br/com/higorcraco/verx_task_api/dto/task/UpdateTaskRequest.java
+++ src/main/java/br/com/higorcraco/verx_task_api/dto/task/UpdateTaskRequest.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 import br.com.higorcraco.verx_task_api.domain.enums.TaskStatus;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateTaskRequest(
 
+        @NotBlank(message = "O título é obrigatório")
         @Size(min = 3, max = 120, message = "O título deve ter entre 3 e 120 caracteres")
         String title,
 
